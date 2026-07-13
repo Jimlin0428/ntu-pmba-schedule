b4bd93e96793d40e5989ecdd7c8043161bd64cb2
Jimlin0428 <48312635+Jimlin0428@users.noreply.github.com>
2026-07-13 11:45:17 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,12 @@
-export type ClassGroup = "A" | "B";
+export type ClassGroup = "PMBA_A" | "PMBA_B" | "PMLBA_A" | "PMLBA_B" | "PMBM_A" | "PMBM_B";
 
 export type Course = {
   name: string;
   teacher: string;
   hours: number;
   classroom: string;
-  classType: "A" | "B" | "all";
+  // 這裡把 classType 擴充，允許接收 "A"、"B"、"ALL" 和 "PMLBA"
+  classType: "A" | "B" | "ALL" | "PMLBA" | "all"; 
   isSpecial?: boolean;
 };
 
