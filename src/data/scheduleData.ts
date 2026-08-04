4f6488e101ae8d5468cc2989f863f1838f0bdc00
Jimlin0428 <48312635+Jimlin0428@users.noreply.github.com>
2026-08-04 10:00:59 +0800

    Update scheduleData.ts

--- src/data/scheduleData.ts
+++ src/data/scheduleData.ts
@@ -1,4 +1,4 @@
 import type { ScheduleDay } from "../types";
-import rawData from "./scheduleData.json";
 
-export const scheduleData = rawData as ScheduleDay[];
+export const scheduleData: ScheduleDay[] = 
+/* 請把剛剛轉好的 JSON 陣列直接貼在這裡 (以 [ 開頭，以 ] 結尾) */;
